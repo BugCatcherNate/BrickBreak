7b8bcc16c1dd3f3018323965cd5f1b68630e01c6
BugCatcherNate <natetthompson@gmail.com>
2019-07-14 18:01:42 -0500

    Added obj loading

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -7,6 +7,7 @@ import models.TexturedModel;
 import org.joml.Vector3f;
 import org.lwjgl.glfw.GLFWKeyCallback;
 import renderEngine.Loader;
+import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import physics.utilities;
@@ -32,99 +33,23 @@ public class MainGameLoop {
 
 		Renderer renderer = new Renderer(shader);
 
-		float[] vertices = {
-				//-0.5f, 0.5f, 0f,
-				//-0.5f,-0.5f, 0f,
-				//0.5f,-0.5f, 0f,
-				//0.5f, 0.5f, 0f
-				0.5f,0.5f,-0.5f,
-				-0.5f,0.5f,-0.5f,
-				-0.5f,0.5f,0.5f,
-				0.5f,0.5f,0.5f,
-				0.5f,-0.5f,0.5f,
-				-0.5f,-0.5f,0.5f,
-				-0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,-0.5f,
-				0.5f,0.5f,0.5f,
-				-0.5f,0.5f,0.5f,
-				-0.5f,-0.5f,0.5f,
-				0.5f,-0.5f,0.5f,
-				0.5f,-0.5f,-0.5f,
-				-0.5f,-0.5f,-0.5f,
-				-0.5f,0.5f,-0.5f,
-				0.5f,0.5f,-0.5f,
-				-0.5f,0.5f,0.5f,
-				-0.5f,0.5f,-0.5f,
-				-0.5f,-0.5f,-0.5f,
-				-0.5f,-0.5f,0.5f,
-				0.5f,0.5f,-0.5f,
-				0.5f,0.5f,0.5f,
-				0.5f,-0.5f,0.5f,
-				0.5f,-0.5f,-0.5f
-
-		};
 
 
+		RawModel model = OBJLoader.loadObjModel("stall", loader);
 
-		int[] indices = {
-				//0,1,3,
-				//3,1,2
-				0,  1,  2,  0,  2,  3,   //front
-				4,  5,  6,  4,  6,  7,   //right
-				8,  9,  10, 8,  10, 11,  //back
-				12, 13, 14, 12, 14, 15,  //left
-				16, 17, 18, 16, 18, 19,  //upper
-				20, 21, 22, 20, 22, 23
-
-
-		};
-
-		float[] textureCoords = {
-
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0
-
-		};
-
-
-
-		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-
-		ModelTexture texture = new ModelTexture(loader.loadTexture("crate"));
+		ModelTexture texture = new ModelTexture(loader.loadTexture("stallTexture"));
 		TexturedModel texturedModel = new TexturedModel(model,texture);
 
 		Entity entity = new Entity(model, texturedModel, new Vector3f(0f,0,-5),0,0,0,1);
 
 
-		float zoom = 0.0002f;
+		float zoom = 0.001f;
 		while(!glfwWindowShouldClose(window)){
 			glfwPollEvents();
 
 
-			entity.increaseRotation(0.0002f, 0.0002f,0);
-			if(entity.getPosition().z < -15 | entity.getPosition().z > -1 ) {
+			entity.increaseRotation(0f, 0.0005f,0);
+			if(entity.getPosition().z < -150 | entity.getPosition().z > -1 ) {
 				zoom *= -1;
 			}
 			entity.increasePosition(0,0,zoom);
